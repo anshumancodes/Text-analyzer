0ad9b308a5c3cef38e05ba3178aabd39c3101c95
Anshumancdx <anshumancodes1510@gmail.com>
2023-04-23 23:29:56 +0530

    count , uppercase , lowercase functions done in js file

--- style & logic/logic.js
+++ style & logic/logic.js
@@ -1,4 +1,17 @@
-console.log("script works")
-Uppercase.addeventlister("click",()=>{
-    
+let textareainp=document.querySelector(".userinputtext");
+// upper
+let Uppercase=document.querySelector(".Uppercase");
+Uppercase.addEventListener("click",()=>{
+    textareainp.value=textareainp.value.toUpperCase()
 })
+// lower
+let lowercase=document.querySelector(".lowercase");
+lowercase.addEventListener("click",()=>{
+    textareainp.value=textareainp.value.toLowerCase()
+})
+// word count
+let wordcount=document.querySelector(".countno");
+textareainp.addEventListener("input",()=>{
+    wordcount.innerHTML=textareainp.value.trim().split(" ").length
+})
+
